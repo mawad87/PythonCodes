99f5de0de083ce6fdf5046ce47b46f619b9f1c1e
mawad87 <mostafa.awad.saleh@gmail.com>
2017-08-25 12:03:15 +0100

    Example 11 Learn Py Hard Way

---
+++ EX11.py
@@ -0,0 +1,14 @@
+print "How old are you ?"
+
+age = raw_input()
+
+print "How tall are you ?"
+
+height = raw_input()
+
+print "How much do you weigh ?"
+
+weight = raw_input()
+
+print "So, you're %d years old, %f tall and %f heavy" %(
+	   age, height, weight)
