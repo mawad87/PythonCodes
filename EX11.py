5516bfa6c2711cdc9d5989610345a2b841e75e00
mawad87 <mostafa.awad.saleh@gmail.com>
2017-08-28 12:13:31 +0100

    Updated EX11

--- EX11.py
+++ EX11.py
@@ -1,5 +1,9 @@
 print "How old are you ?"
 
+## by default the output of raw_input is string
+
+## so with the below I used int() function to convert the input into numbers
+
 age = int(raw_input())
 
 print "How tall are you ?"
@@ -9,6 +13,9 @@ height = float(raw_input())
 print "How much do you weigh ?"
 
 weight = float(raw_input())
+
+# %d refer to integer
+# %f refer to float number
 
 print "So, you're %d years old, %f tall and %f heavy" %(
 	   age, height, weight)
