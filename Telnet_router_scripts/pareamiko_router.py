c74c20f17ec9d7337177ec8e441900e2cd30233d
mawad87 <mostafa.awad.saleh@gmail.com>
2019-10-18 22:40:59 +0100

    Update pareamiko_router.py

--- Telnet_router_scripts/pareamiko_router.py
+++ Telnet_router_scripts/pareamiko_router.py
@@ -5,11 +5,12 @@ import time
 HOST = "172.168.10.111"
 USER = "cisco"
 PASS = "cisco"
+proxy = None
 
 ssh_client = paramiko.SSHClient()
 
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_client.connect(HOST,USER,PASS)
+ssh_client.connect(HOST,USER,PASS,sock=proxy)
 
 ssh_connecion = ssh_client.invoke_shell()
 
