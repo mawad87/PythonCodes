79584acada41965d77f735dd4a80514ea28d86a4
mawad87 <mostafa.awad.saleh@gmail.com>
2019-10-18 10:02:38 +0100

    Update pareamiko_router.py

--- Telnet_router_scripts/pareamiko_router.py
+++ Telnet_router_scripts/pareamiko_router.py
@@ -7,9 +7,11 @@ USR = input("Enter your Username: ")
 PASS = getpass.getpass()
 
 ssh_client = paramiko.SSHClient()
-ssh_connecion = ssh_client.invoke_shell()
+
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client.connect(hostname=HOST,username=USR,password=PASS)
+
+ssh_connecion = ssh_client.invoke_shell()
 
 ssh_connecion.send("show ip route\n")
 
