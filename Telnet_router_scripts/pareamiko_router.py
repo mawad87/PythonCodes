6bf42abbb69184d5fa111b3eb34e44d188d74269
mawad87 <mostafa.awad.saleh@gmail.com>
2019-10-18 22:50:55 +0100

    Update pareamiko_router.py

--- Telnet_router_scripts/pareamiko_router.py
+++ Telnet_router_scripts/pareamiko_router.py
@@ -3,20 +3,25 @@ import getpass
 import time
 
 HOST = "172.168.10.111"
-USER = "cisco"
-PASS = "cisco"
+username = raw_input("Please enter your username: ")
+password = getpass()
 
 ssh_client = paramiko.SSHClient()
 ssh_client.load_system_host_keys()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_client.connect(HOST,username=USER,password=PASS,look_for_keys=False)
+ssh_client.connect(HOST,username=username,password=password,look_for_keys=False)
 
 ssh_connecion = ssh_client.invoke_shell()
 
 ssh_connecion.send("show ip route\n")
 
-output = ssh_connecion.recv(11111).decode(encoding="utf-8")
+output = ssh_connecion.recv(65535).decode(encoding="utf-8")
 
-print(output)
+print output
+
+ssh_client.send("show ip int brief\n")
+time.sleep(.5)
+output = ssh_client.recv(65535)
+print output
 
 ssh_connecion.close
